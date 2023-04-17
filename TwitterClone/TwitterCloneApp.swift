16d70f73f6a0c280f165ad9389c571b28b0cc956
404ibra <ibra.aktass@gmail.com>
2023-04-17 13:58:22 +0300

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by İbrahim Aktaş on 17.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
