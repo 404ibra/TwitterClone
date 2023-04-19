5072d968ab2721358cc7ce5ac8bf179b6fd14a58
404ibra <ibra.aktass@gmail.com>
2023-04-19 18:54:18 +0300

    login view is completed

--- TwitterClone/Core/Authhenticion/Views/LoginView.swift
+++ TwitterClone/Core/Authhenticion/Views/LoginView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
     var body: some View {
         VStack{
             
@@ -25,10 +27,60 @@ struct LoginView: View {
             .background(.blue)
             .foregroundColor(.white)
             .clipShape(RoundedShape(corners: [.bottomRight]))
+ 
+            VStack(spacing: 40){
+                TextField("Email", text: $email)
+                TextField("Password", text: $password)
+            }.padding(.horizontal, 32)
+                .padding(.top,44)
+            
+            HStack{
+                Spacer()
+                NavigationLink {
+                    Text("Reset Password View")
+                } label: {
+                    Text("Forgot Password")
+                        .font(.caption)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.blue)
+                        .padding(.top)
+                        .padding(.trailing, 24)
+                }
+            }
+            Button {
+                print("Sign in")
+            } label: {
+                Text("Sign In")
+                    .font(.headline)
+                    .frame(width:340, height: 50)
+                    .background(.blue)
+                    .clipShape(Capsule())
+                    .foregroundColor(.white)
+                    .padding()
+            }
+            .shadow(color: .gray.opacity(0.5),radius: 10, x:0, y:0)
+
             Spacer()
+            NavigationLink {
+                RegisterView()
+                    .navigationBarHidden(true)
+            } label: {
+                HStack{
+                    Text("Don't have an account ? ")
+                        .font(.footnote)
+                    Text("Sign Up ")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                }
+                .padding(.bottom, 32)
+                .foregroundColor(.blue)
+            }
+
         }//VStack
         .ignoresSafeArea()
-      
+        .navigationBarHidden(true)
+        
+     
         }
 }
 
