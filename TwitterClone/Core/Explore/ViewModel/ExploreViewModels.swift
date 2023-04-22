575c7f5076c1434582fd60345b9c2ae9edd3816a
404ibra <ibra.aktass@gmail.com>
2023-04-22 13:50:11 +0300

    explore view model completed

--- TwitterClone/Core/Explore/ViewModel/ExploreViewModels.swift
+++ TwitterClone/Core/Explore/ViewModel/ExploreViewModels.swift
@@ -9,6 +9,21 @@ import Foundation
 
 class ExploreViewModel: ObservableObject {
     @Published var users = [User]()
+    @Published var searchText = ""
+    
+    var searchableUsers: [User] {
+        if searchText.isEmpty {
+            return users
+        }else {
+            let lowerCaseQuery = searchText.lowercased()
+            
+            return users.filter({
+                $0.username.contains(lowerCaseQuery) ||
+                $0.fullname.lowercased().contains(lowerCaseQuery)
+            })
+        }
+    }
+    
     let service =  UserService()
     
     
